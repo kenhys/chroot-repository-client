84d55c74e3946d9c6d53d2067fb5c42f0151cb57
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-11 00:28:05 +0900

    Implement check address command

--- lib/chroot/repository/client/check.rb
+++ lib/chroot/repository/client/check.rb
@@ -1,12 +1,17 @@
 require 'thor'
+require 'chroot/repository/client/helper'
 
 module Chroot
   module Repository
     module Client
       class Check < Thor
 
+        include Chroot::Repository::Client::Helper
+
         desc "address", "Check upstream address of chroot"
         def address
+          iface_address = get_interface_address
+          puts iface_address
         end
 
         desc "install", "Check installed packages under chroot"

868dbbfb6d1dcd4fa9e4a6fccdb72a9ed694a6de
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-11 00:21:11 +0900

    Remove needless nested bracket from template

--- lib/chroot/repository/client/config.rb
+++ lib/chroot/repository/client/config.rb
@@ -8,20 +8,18 @@ module Chroot
 
         desc "setup", "Setup initial configuration"
         def setup
-          template = [
-                      {
-                        'interface' => 'eth0',
-                        'chroot_dir' => '/var/lib/chroot',
-                        'deb' => {
-                          'codes' => ['squeeze', 'wheezy', 'jessie', 'unstable'],
-                          'arch' => ['i386', 'amd64']
-                        },
-                        'rpm' => {
-                          'dists' => ['centos-5', 'centos-6', 'fedora-19'],
-                          'arch' => ['i386', 'x86_64']
-                        }
-                      }
-                     ]
+          template = {
+            'interface' => 'eth0',
+            'chroot_dir' => '/var/lib/chroot',
+            'deb' => {
+              'codes' => ['squeeze', 'wheezy', 'jessie', 'unstable'],
+              'arch' => ['i386', 'amd64']
+            },
+            'rpm' => {
+              'dists' => ['centos-5', 'centos-6', 'fedora-19'],
+              'arch' => ['i386', 'x86_64']
+            }
+          }
           config_dir = File.expand_path("~/.chroot-repository-client")
           Dir.mkdir(config_dir) unless File.exist?(config_dir)
           Dir.chdir(config_dir) do

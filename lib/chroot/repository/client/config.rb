cbdd36da5a2224d0c5cede428de5d858160d3758
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-11 00:04:36 +0900

    Add functionality to generate configuration file

--- lib/chroot/repository/client/config.rb
+++ lib/chroot/repository/client/config.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'yaml'
 
 module Chroot
   module Repository
@@ -7,6 +8,27 @@ module Chroot
 
         desc "setup", "Setup initial configuration"
         def setup
+          template = [
+                      {
+                        'interface' => 'eth0',
+                        'chroot_dir' => '/var/lib/chroot',
+                        'deb' => {
+                          'codes' => ['squeeze', 'wheezy', 'jessie', 'unstable'],
+                          'arch' => ['i386', 'amd64']
+                        },
+                        'rpm' => {
+                          'dists' => ['centos-5', 'centos-6', 'fedora-19'],
+                          'arch' => ['i386', 'x86_64']
+                        }
+                      }
+                     ]
+          config_dir = File.expand_path("~/.chroot-repository-client")
+          Dir.mkdir(config_dir) unless File.exist?(config_dir)
+          Dir.chdir(config_dir) do
+            File.open("config.yaml", "w+") do |file|
+              file.puts(YAML.dump(template))
+            end
+          end
         end
 
       end

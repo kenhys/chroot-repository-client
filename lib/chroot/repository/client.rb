8c40e4d6f3e38de597a92e40bfc94c18b3ec2bc1
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-10 21:04:32 +0900

    Add initial template files

---
+++ lib/chroot/repository/client.rb
@@ -0,0 +1,9 @@
+require "chroot/repository/client/version"
+
+module Chroot
+  module Repository
+    module Client
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/chroot/repository/client/version.rb
@@ -0,0 +1,7 @@
+module Chroot
+  module Repository
+    module Client
+      VERSION = "0.0.1"
+    end
+  end
+end

be84fa20e291c3adaa29c2d61bacf375cf4d4b6e
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-12 00:32:58 +0900

    Add codes/distributions related constants

--- lib/chroot/repository/client/const.rb
+++ lib/chroot/repository/client/const.rb
@@ -6,6 +6,15 @@ module Chroot
       CONFIG_DIR = "~/.chroot-repository-client"
       CONFIG_FILE = "config.yaml"
       CONFIG_PATH = "#{CONFIG_DIR}/#{CONFIG_FILE}"
+
+      CODES = ["squeeze", "wheezy", "jessie", "unstable",
+               "lucid", "precise", "quantal", "raring"]
+      CODES_ARCH = ["i386", "amd64"]
+
+      DISTS = ["centos", "centos", "fedora"]
+      DISTS_ARCH = ["i386", "x86_64"]
+
+      CENTOS_VERSIONS = ["5", "6"]
     end
   end
 end

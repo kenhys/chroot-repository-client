d7e78f75946d32770cca10b8f584faed519a1d46
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-11 23:52:28 +0900

    Add constant

---
+++ lib/chroot/repository/client/const.rb
@@ -0,0 +1,11 @@
+
+module Chroot
+  module Repository
+    module Client
+
+      CONFIG_DIR = "~/.chroot-repository-client"
+      CONFIG_FILE = "config.yaml"
+      CONFIG_PATH = "#{CONFIG_DIR}/#{CONFIG_FILE}"
+    end
+  end
+end

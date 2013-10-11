73da240eca9c716cd8bf22c4dbb5b602acc32d9b
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-11 23:52:40 +0900

    Require constant

--- lib/chroot/repository/client.rb
+++ lib/chroot/repository/client.rb
@@ -1,4 +1,5 @@
 require "chroot/repository/client/version"
+require "chroot/repository/client/const"
 require "chroot/repository/client/check"
 require "chroot/repository/client/config"
 require "chroot/repository/client/install"

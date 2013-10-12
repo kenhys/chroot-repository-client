4fc46bcbf3c0973554a02c7ad7847902fe38cb91
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-13 00:50:38 +0900

    Add provide command for mroonga

--- lib/chroot/repository/client/package/mroonga.rb
+++ lib/chroot/repository/client/package/mroonga.rb
@@ -45,6 +45,41 @@ EOS
           end
         end
       end
+
+      class Check < Thor
+
+        desc "provide", "Check provided packages"
+        option :codes
+        option :dists
+        option :arch
+        def provide
+          codes = get_option_codes(options)
+          archs = get_option_arch(options)
+
+          codes.each do |code|
+            archs.each do |arch|
+              content = <<-EOS
+apt-get update > /dev/null 2>&1
+apt-cache show mysql-server | grep "Version:" | head -1 > /tmp/mysql-version
+EOS
+              root_dir = get_chroot_dir + "/#{code}-#{arch}"
+              script_path = "#{root_dir}/tmp/check-provided-mysql.sh"
+              File.open(script_path, "w+", 0755) do |file|
+                file.puts(content)
+              end
+              host = "#{code}-#{arch}"
+              basename = File.basename(script_path)
+              command = "sudo chname #{host} chroot #{root_dir} /tmp/#{basename}"
+              raw = `#{command}`
+              File.open("#{root_dir}/tmp/mysql-version") do |file|
+                content = file.read.chomp
+                printf "%8s %5s mysql:%s\n", code, arch, content.split(":")[1]
+
+              end
+            end
+          end
+        end
+      end
     end
   end
 end

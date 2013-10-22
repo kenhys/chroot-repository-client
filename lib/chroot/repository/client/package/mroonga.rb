df967c842c70d22ebb7ae57dea07e76ab5053e08
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-22 13:55:55 +0900

    Show check build result for rpms

--- lib/chroot/repository/client/package/mroonga.rb
+++ lib/chroot/repository/client/package/mroonga.rb
@@ -17,6 +17,25 @@ module Chroot
                 end
               end
             end
+
+            dists = get_option_dists(options)
+            darch = get_option_darch(options)
+            dists.each do |dist|
+              versions = CENTOS_VERSIONS if dist == "centos"
+              versions = ["19"] if dist == "fedora"
+              versions.each do |version|
+                darch.each do |arch|
+                  base_version=`cat ../version`
+                  sets=`find yum/repositories/#{dist}/#{version} -name "*#{base_version}*.rpm" | grep #{arch}`
+                  printf "%10s %7s %s => ", "#{dist}-#{version}", arch, base_version
+                  if not sets.length.zero?
+                    printf "%2d rpms\n", sets.split("\n").length
+                  else
+                    printf "%2d rpms\n", 0
+                  end
+                end
+              end
+            end
           end
 
           def check_install_mroonga(options)

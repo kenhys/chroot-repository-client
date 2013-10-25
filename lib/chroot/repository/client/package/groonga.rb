2622b6993ac3fa4c67352d3dee3440c60311d062
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-25 14:36:54 +0900

    Get groonga directory from configuration file

--- lib/chroot/repository/client/package/groonga.rb
+++ lib/chroot/repository/client/package/groonga.rb
@@ -10,10 +10,13 @@ module Chroot
             codes = get_option_codes(options)
             archs = get_option_arch(options)
 
+            config = get_config_file
+            groonga_dir = config['groonga_dir']
+            base_version=`cat #{groonga_dir}/base_version`
             codes.each do |code|
               archs.each do |arch|
-                base_version=`cat ../base_version`
-                sets=`find apt/repositories -name "*#{base_version}*.deb" | grep #{code} | grep #{arch}`
+                repository_dir = File.join(groonga_dir, "packages/apt/repositories")
+                sets=`find #{repository_dir} -name "*#{base_version}*.deb" | grep #{code} | grep #{arch}`
                 if not sets.length.zero?
                   printf "%8s %5s %s => %2d debs\n", code, arch, base_version, sets.split("\n").length
                 else
@@ -29,8 +32,8 @@ module Chroot
               versions = ["19"] if dist == "fedora"
               versions.each do |version|
                 darch.each do |arch|
-                  base_version=`cat ../base_version`
-                  sets=`find yum/repositories/#{dist}/#{version} -name "*#{base_version}*.rpm" | grep #{arch}`
+                  repository_dir = File.join(groonga_dir, "packages/yum/repositories")
+                  sets=`find #{repository_dir}/#{dist}/#{version} -name "*#{base_version}*.rpm" | grep #{arch}`
                   printf "%10s %7s %s => ", "#{dist}-#{version}", arch, base_version
                   if not sets.length.zero?
                     printf "%2d rpms\n", sets.split("\n").length
